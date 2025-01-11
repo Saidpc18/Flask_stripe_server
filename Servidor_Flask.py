82f59991f6d73c7a1c2226d63552cbffd21b7739
Said Perales <said.perales.18@gmail.com>
2025-01-10 18:17:32 -0600

    cambio en requerimientos

--- Servidor_Flask.py
+++ Servidor_Flask.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Configura tu clave secreta de Stripe y el webhook secret
 stripe.api_key = "Merfosis22"  # Reemplaza con tu clave secreta real
-webhook_secret = "whsec_Me48IFO1D1DmHKGtimdnw8ALuVMTXRd8"  # Reemplaza con tu webhook secret
+webhook_secret = "whsec_4QAnSKkUNDYAoOSfmURtHNelKARrQw5k"  # Reemplaza con tu webhook secret
 
 # Archivo de usuarios
 usuarios_archivo = "usuarios.json"
