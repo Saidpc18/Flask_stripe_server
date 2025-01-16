59bca1ea9720a59c4612886cc0c7d3cbe8ddc668
Said Perales <said.perales.18@gmail.com>
2025-01-16 17:31:18 -0600

    cambio servidor

--- prueba.py
+++ prueba.py
@@ -1,3 +1,20 @@
-import psycopg2
-import marshmallow
-print("Ambos módulos están instalados correctamente.")
+import stripe
+
+stripe.api_key = 'sk_test_51QfUyjG4Og1KI6OFFdnGycLum4dgpBqyWg2fSSRSPcsW8rcOc6s6p1R2AnAw17UnkVwUntnqrjJETYKbW0Yte2qn00lGjpWUoW'
+
+session = stripe.checkout.Session.create(
+    payment_method_types=['card'],
+    line_items=[{
+        'price_data': {
+            'currency': 'usd',
+            'product_data': {
+                'name': 'Nombre del Producto',
+            },
+            'unit_amount': 1000,  # Monto en centavos (1000 = $10.00)
+        },
+        'quantity': 1,
+    }],
+    mode='payment',
+    success_url='https://flask-stripe-server.onrender.com/success?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url='https://flask-stripe-server.onrender.com/cancel',
+)
