d9a91c3cb23212f2354007d5268e25751982020a
Said Perales <said.perales.18@gmail.com>
2025-01-18 01:51:12 -0600

    cambio servidor

--- prueba.py
+++ prueba.py
@@ -1,18 +1,5 @@
-import requests
+import psycopg2
 
-# Define la URL de tu endpoint
-url = "https://flask-stripe-server.onrender.com/create-checkout-session"  # Cambia a tu URL de producción si aplica
-
-# Datos a enviar en el cuerpo de la solicitud
-data = {
-    "user": "prueba_usuario"
-}
-
-# Envía la solicitud POST
-response = requests.post(url, json=data)
-
-# Muestra la respuesta
-if response.status_code == 200:
-    print("URL de Stripe Checkout:", response.json().get("url"))
-else:
-    print("Error:", response.status_code, response.json())
+connection = psycopg2.connect("postgresql://postgres:woTCfdaWchoxcsKAmCaAxOBzHusEdLLj@postgres.railway.internal:5432/railway")
+print("Conexión exitosa")
+connection.close()
