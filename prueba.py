2693cf298dfcae6a9a5491ce10b52fe40114af59
Said Perales <said.perales.18@gmail.com>
2025-01-18 02:00:12 -0600

    cambio servidor

--- prueba.py
+++ prueba.py
@@ -1,5 +1,5 @@
 import psycopg2
 
-connection = psycopg2.connect("postgresql://postgres:woTCfdaWchoxcsKAmCaAxOBzHusEdLLj@junction.proxy.rlwy.net:5432/railway")
+connection = psycopg2.connect("postgresql://postgres:woTCfdaWchoxcsKAmCaAxOBzHusEdLLj@junction.proxy.rlwy.net:19506/railway")
 print("Conexión exitosa")
 connection.close()
