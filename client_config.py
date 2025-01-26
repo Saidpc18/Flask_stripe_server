29e914cf7aef6e929ed80ac511691a65b5b683f8
Said Perales <said.perales.18@gmail.com>
2025-01-26 17:34:24 -0600

    cambio servidor

--- client_config.py
+++ client_config.py
@@ -1,18 +1,7 @@
 class ClientConfig(object):
------BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAufB7ZSvH6qQeiVp0ho0b
-uD29v2yLWCavbrPiE8xu5vjYWg86XRMz8qNO7UptMIfUXgDYhxvEV7N/xMYJGZOc
-ezowAFchVsAq4iOYlgU1gpOje8ew5Flz/hnScheU3Bc8wIXHLWZrbV6LwN7L8snl
-OSk1EHlVWYV4Zh/s6405dIcOxOcZX/g3ViSJgMLcce+OgCHFMMPuN8FpTNUZsgR4
-T6dFw9CrazETxNp5TfIC2BnipvIrXPAQu+BbCWrI/8S4bvw2u2zB+evBShZnQ/j4
-N8lH4NG+ZtRVDZFz2kTVgZpSw4oySBowypcpUnPmQfFDV1zi6wXzmq9mXoLP9ig+
-qQIDAQAB
------END PUBLIC KEY-----
-
-
-'
+    PUBLIC_KEY = 'None'
     APP_NAME = 'Vinder'
-    COMPANY_NAME = 'Said_Perales'
+    COMPANY_NAME = 'SaidPerales'
     HTTP_TIMEOUT = 30
     MAX_DOWNLOAD_RETRIES = 3
-    UPDATE_URLS = ['https://github.com/Saidpc18/Flask_stripe_server/releases']
+    UPDATE_URLS = ['https://github.com/Saidpc18/Flask_stripe_server/releases', 'https://flask-stripe-server.onrender.com']
