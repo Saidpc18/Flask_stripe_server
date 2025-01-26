491c47a8c4c0e9fa360fcbbb0d40842b3e46caca
Said Perales <said.perales.18@gmail.com>
2025-01-26 17:01:30 -0600

    cambio servidor

--- client_config.py
+++ client_config.py
@@ -1,6 +1,17 @@
 class ClientConfig(object):
-    PUBLIC_KEY = 'None'
-    APP_NAME = 'Repuve'
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAufB7ZSvH6qQeiVp0ho0b
+uD29v2yLWCavbrPiE8xu5vjYWg86XRMz8qNO7UptMIfUXgDYhxvEV7N/xMYJGZOc
+ezowAFchVsAq4iOYlgU1gpOje8ew5Flz/hnScheU3Bc8wIXHLWZrbV6LwN7L8snl
+OSk1EHlVWYV4Zh/s6405dIcOxOcZX/g3ViSJgMLcce+OgCHFMMPuN8FpTNUZsgR4
+T6dFw9CrazETxNp5TfIC2BnipvIrXPAQu+BbCWrI/8S4bvw2u2zB+evBShZnQ/j4
+N8lH4NG+ZtRVDZFz2kTVgZpSw4oySBowypcpUnPmQfFDV1zi6wXzmq9mXoLP9ig+
+qQIDAQAB
+-----END PUBLIC KEY-----
+
+
+'
+    APP_NAME = 'Vinder'
     COMPANY_NAME = 'Said_Perales'
     HTTP_TIMEOUT = 30
     MAX_DOWNLOAD_RETRIES = 3
