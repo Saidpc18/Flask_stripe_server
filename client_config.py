5d5bce78dbba750122cff4fefead4d0f50fdb7c0
Said Perales <said.perales.18@gmail.com>
2025-01-25 22:03:51 -0600

    cambio servidor

--- client_config.py
+++ client_config.py
@@ -1,7 +1,7 @@
 class ClientConfig(object):
     PUBLIC_KEY = 'None'
     APP_NAME = 'Vinder'
-    COMPANY_NAME = 'Said'
+    COMPANY_NAME = 'Said_Perales'
     HTTP_TIMEOUT = 30
     MAX_DOWNLOAD_RETRIES = 3
-    UPDATE_URLS = ['https://api.github.com/repos/Saidpc18/Flask_stripe_server']
+    UPDATE_URLS = ['https://github.com/Saidpc18/Flask_stripe_server/releases']
