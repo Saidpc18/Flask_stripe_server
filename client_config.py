3720d0568e1c119407e0050a8ae66248f6add420
Said Perales <said.perales.18@gmail.com>
2025-01-26 23:51:23 -0600

    cambio servidor

--- client_config.py
+++ client_config.py
@@ -1,5 +1,7 @@
 class ClientConfig(object):
-    PUBLIC_KEY = 'None'
+     # Leer claves manualmente
+    with open(".pyupdater/keys/keys.pub", "r") as pub_key_file:
+        PUBLIC_KEY = pub_key_file.read()
     APP_NAME = 'Vinder'
     COMPANY_NAME = 'Said_P'
     HTTP_TIMEOUT = 30
