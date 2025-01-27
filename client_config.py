8130ff2cd9c9b3b6880eb41ef993f88325dd77ae
Said Perales <said.perales.18@gmail.com>
2025-01-26 19:00:49 -0600

    cambio servidor

--- client_config.py
+++ client_config.py
@@ -1,6 +1,14 @@
 class ClientConfig(object):
-    with open(".pyupdater/keys/keys.pub", "r") as f:
-        PUBLIC_KEY = f.read()
+    PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1aMERyfzkzqBtkv4Tg/j
+G+U5eNNvV9nCmnvYT9SJD+PunYFJVnmcwTgf1kBvh/YjEp7IzzfNzcWFYvjuzrEQ
+taiRh1960ox11UZhQa8S8zFv4r7q9qBToBUf8o7XM37Cn+gCTTho+kA1zjuMjmYv
+Mt6Py6q4+2yUKIWv+PmQ7vFEPzYzUDkN5hAahPAywkoQOAOgLlB435eQcfIYSOux
+YypaC+8VjcN0r3B5xwi+nlmQtOPZTh1MmlJK4uwofvxCjK+zwb7Fo80fekit/cnG
+CSI4cdT/YT6/IHTnK16YDaQ18UiAT4rvfsqrPQLLfLBTWvujszWOutHC702FUoxd
+qQIDAQAB
+-----END PUBLIC KEY-----"""
+
     APP_NAME = 'Vinder'
     COMPANY_NAME = 'SaidPerales'
     HTTP_TIMEOUT = 30
