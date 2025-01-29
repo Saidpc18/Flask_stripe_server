f490620f33e6c89eeb68e46a0200798b64c50279
Said Perales <said.perales.18@gmail.com>
2025-01-29 12:52:04 -0600

    cambio servidor

--- client_config.py
+++ client_config.py
@@ -1,19 +1,7 @@
-import os
-
 class ClientConfig(object):
-    # Leer claves manualmente con verificación de existencia
-    try:
-        with open(".pyupdater/keys/keys.pub", "r") as pub_key_file:
-            PUBLIC_KEY = pub_key_file.read()
-    except FileNotFoundError:
-        PUBLIC_KEY = None  # O maneja el error según sea necesario
-
-    # Configuración de la aplicación
+    PUBLIC_KEY = 'sJJiVQOxmD7BZ3gBgJYgERPIdNjTOfAoTZekdqu00Ls'
     APP_NAME = 'Vinder'
     COMPANY_NAME = 'SaidPerales'
     HTTP_TIMEOUT = 30
     MAX_DOWNLOAD_RETRIES = 3
-    UPDATE_URLS = [
-        'https://github.com/Saidpc18/Flask_stripe_server/releases',
-        'https://flask-stripe-server.onrender.com'
-    ]
+    UPDATE_URLS = ['https://github.com/Saidpc18/Flask_stripe_server/releases', 'https://flask-stripe-server.onrender.com']
